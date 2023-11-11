d5e14a1c214b76688c202b59fa10b2201774c414
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-10 22:24:02 -0600

    Update Ejercicio02.cpp
    Signed-off-by: 1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>

--- PAT_Parcial_Reposicion/Ejercicio02.cpp
+++ PAT_Parcial_Reposicion/Ejercicio02.cpp
@@ -1,23 +1,5 @@
 #include "Ejercicio02.h"
 
-Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k)
-{
-    Node<char>* current = head;
-    int count = 0;
-
-    while (current != nullptr && count < k) {
-        current = current->next;
-        count++;
-    }
-
-    if (count == k) {
-        current = reverseKNodes(head, k);
-        head->next = reverseKGroup(current, k);
-        return current;
-    }
-    return head;
-}
-
 Node<char>* Ejercicio02::reverseKNodes(Node<char>* head, int k) {
     Node<char>* prev = nullptr;
     Node<char>* current = head;
