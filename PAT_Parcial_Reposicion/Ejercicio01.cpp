cdd487c00b63bb1bed1475dfd5a61d52e08ab019
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-10 20:53:11 -0600

    Update Ejercicio01.cpp
    Signed-off-by: 1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>

--- PAT_Parcial_Reposicion/Ejercicio01.cpp
+++ PAT_Parcial_Reposicion/Ejercicio01.cpp
@@ -3,25 +3,25 @@
 Node<int>* Ejercicio01::addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2)
 {
     Node<int>* dummyHead = new Node<int>(0);
-    Node<int>* current = dummyHead;  
-    int carry = 0;  
+    Node<int>* current = dummyHead;
+    int carry = 0;
 
     while (l1 || l2) {
         int x = (l1) ? l1->data : 0;
         int y = (l2) ? l2->data : 0;
         int sum = x + y + carry;
 
-        carry = sum / 10;  
-        current->next = new Node<int>(sum % 10); 
-        current = current->next;  
+        carry = sum / 10;
+        current->next = new Node<int>(sum % 10);
+        current = current->next;
 
         if (l1) l1 = l1->next;
         if (l2) l2 = l2->next;
     }
 
     if (carry > 0) {
-        current->next = new Node<int>(carry); 
+        current->next = new Node<int>(carry);
     }
 
-    return dummyHead->next; 
+    return dummyHead->next;
 }
