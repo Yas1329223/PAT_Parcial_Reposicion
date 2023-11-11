1c6b73e7e003f9efbda1b14489a7586fba030a80
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-10 21:36:29 -0600

    Update Ejercicio01.cpp
    Signed-off-by: 1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>

--- PAT_Parcial_Reposicion/Ejercicio01.cpp
+++ PAT_Parcial_Reposicion/Ejercicio01.cpp
@@ -2,26 +2,34 @@
 
 Node<int>* Ejercicio01::addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2)
 {
-    Node<int>* dummyHead = new Node<int>(0);
-    Node<int>* current = dummyHead;
-    int carry = 0;
+     Node<int>* result = nullptr; // Inicializamos la lista resultante como vacía
+    Node<int>* current = nullptr; // Inicializamos el nodo actual como vacío
+    int carry = 0; // Inicializamos el acarreo como cero
 
-    while (l1 || l2) {
-        int x = (l1) ? l1->data : 0;
-        int y = (l2) ? l2->data : 0;
-        int sum = x + y + carry;
+    while (l1 != nullptr || l2 != nullptr) {
+        int val1 = (l1 != nullptr) ? l1->data : 0;
+        int val2 = (l2 != nullptr) ? l2->data : 0;
+        int sum = val1 + val2 + carry;
 
         carry = sum / 10;
-        current->next = new Node<int>(sum % 10);
-        current = current->next;
 
-        if (l1) l1 = l1->next;
-        if (l2) l2 = l2->next;
+        Node<int>* newNode = new Node<int>(sum % 10);
+        if (result == nullptr) {
+            result = newNode;
+            current = result;
+        } else {
+            current->next = newNode;
+            current = current->next;
+        }
+
+        if (l1 != nullptr) l1 = l1->next;
+        if (l2 != nullptr) l2 = l2->next;
     }
 
     if (carry > 0) {
-        current->next = new Node<int>(carry);
+        Node<int>* newNode = new Node<int>(carry);
+        current->next = newNode;
     }
 
-    return dummyHead->next;
+    return result;
 }
