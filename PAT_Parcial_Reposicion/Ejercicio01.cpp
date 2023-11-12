ab9ecf2c8c748ed2aa687ab388eb5c63c8c93c07
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-12 16:27:55 -0600

    Update Ejercicio01.cpp
    Signed-off-by: 1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>

--- PAT_Parcial_Reposicion/Ejercicio01.cpp
+++ PAT_Parcial_Reposicion/Ejercicio01.cpp
@@ -1,27 +1,53 @@
 #include "Ejercicio01.h"
 
-Node<int>* Ejercicio01::addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2)
+Node<int>* reverseList(Node<int>* head)
 {
-     Node<int>* dummyHead = new Node<int>(0); 
-    Node<int>* current = dummyHead;
-    int carry = 0;
-
-    while (l1 != nullptr || l2 != nullptr) {
-        int x = (l1 != nullptr) ? l1->val : 0;
-        int y = (l2 != nullptr) ? l2->val : 0;
-        int sum = carry + x + y;
-        carry = sum / 10;
-
-        current->next = new Node<int>(sum % 10);
-        current = current->next;
-
-        if (l1 != nullptr) l1 = l1->next;
-        if (l2 != nullptr) l2 = l2->next;
+    Node<int>* current = head;
+    Node<int>* prev = nullptr;
+    Node<int>* next = nullptr;
+    
+    while (current != nullptr)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
     }
-
-    if (carry > 0) {
-        current->next = new Node<int>(carry);
-    }
-
-    return dummyHead->next;
+    
+    return prev;
 }
+
+Node<int>* addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2)
+{
+    Node<int>* l1Reversed = reverseList(l1);
+    Node<int>* l2Reversed = reverseList(l2);
+    
+    Node<int>* l3 = new Node<int>{ 0 };
+    Node<int>* l3Current = l3;
+    int carry = 0;
+    
+    while (l1Reversed != nullptr || l2Reversed != nullptr) {
+        int sum = carry;
+        
+        if (l1Reversed != nullptr) {
+            sum += l1Reversed->value;
+            l1Reversed = l1Reversed->next;
+        }
+        
+        if (l2Reversed != nullptr) {
+            sum += l2Reversed->value;
+            l2Reversed = l2Reversed->next;
+        }
+        
+        l3Current->next = new Node<int>{ sum % 10 };
+        l3Current = l3Current->next;
+        carry = sum / 10;
+    }
+    
+    if (carry > 0) {
+        l3Current->next = new Node<int>{ carry };
+    }
+    
+    return reverseList(l3->next);
+}
+
